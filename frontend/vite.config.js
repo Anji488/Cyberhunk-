81ac5092c8fea995cc0e15a03273fc07b271fe93
Anji488 <s92074854@ousl.lk>
2025-10-12 22:05:33 +0530

    update

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,10 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from 'path'
+import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
+    tailwindcss(),
     react(),
   ],
   server: {
@@ -15,5 +17,5 @@ export default defineConfig({
       "@": path.resolve(__dirname, "src"),
     },
   },
-  base: "/", // ensure correct routing on Vercel
+  base: "/", 
 })
