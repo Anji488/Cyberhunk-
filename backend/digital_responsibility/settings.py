00aa2a449afe3101c8222f6b43ec8a6c98395fc9
Anji488 <s92074854@ousl.lk>
2025-12-06 11:46:37 +0530

    update

--- backend/digital_responsibility/settings.py
+++ backend/digital_responsibility/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-hlqa933e$&irs(+u9)&yemqba(y2!cjw56xy85suu^mv#+n*&x'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["localhost", "127.0.0.1"]
 
